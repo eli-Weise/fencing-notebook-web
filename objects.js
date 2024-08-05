16f0ad8e3ea99881d4367931850742319a284b61
eliWeise <eli@weises.org>
2024-08-04 23:42:31 -0700

    Add "fencer" and "bout" JS objects
    Made the initial Javascript objects for new fencers and bouts
    Updated "new-bout" page to include option for explicit winner in case of
    time running out. In the future, will only show if scores are tied.
    Function has not been tested yet.

---
+++ objects.js
@@ -0,0 +1,52 @@
+/**
+ * Fencer object constructor
+ * @param {string} firstName 
+ * @param {string} lastName 
+ * @param {Enumerator} rating 
+ * @param {Enumerator} hand 
+ * @param {Enumerator} grip 
+ * @param {number} birthYear 
+ * @param {Enumerator} height 
+ * @param {string} notes 
+ */
+function Fencer(firstName, lastName, rating, hand, grip, birthYear, height, notes) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.rating = rating;
+  this.hand = hand;
+  this.grip = grip;
+  this.birthYear = birthYear;
+  this.height = height;
+  this.notes = notes;
+}
+
+/**
+ * Bout object constructor
+ * @param {Fencer} opponent 
+ * @param {number} myScore 
+ * @param {number} opponentScore 
+ * @param {object} myCards 
+ * @param {object} opponentCards 
+ * @param {Enumerator} pCards 
+ * @param {string} notes 
+ */
+function Bout(opponent, winner, myScore, opponentScore, myCards, opponentCards, pCards, notes) {
+  this.opponent = opponent;
+  this.winner = winner;
+  this.myScore = myScore;
+  this.opponentScore = opponentScore;
+  this.myCards = myCards;
+  this.opponentCards = opponentCards;
+  this.pCards = pCards;
+  this.notes = notes;
+
+  this.getWinner = function () {
+    if (this.winner) {
+      console.log(winner + "won!");
+    } else if (myScore >= opponentScore) {
+      console.log("I won!");
+    } else {
+      console.log(opponent + "won!");
+    }
+  }
+}
