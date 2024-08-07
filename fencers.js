dc90f109d137ea35816b61d24ea3d0b537c78ece
eliWeise <eli@weises.org>
2024-08-07 11:59:14 -0700

    Enable "fencer-list" "edit" button
    Edited fencers do not keep their position in list, will sort when
      printing in the future
    Broke radio buttons and checkboxes. They now only return first possible
      value

--- fencers.js
+++ fencers.js
@@ -1,3 +1,12 @@
+const firstName = document.querySelector("#first_name");
+const lastName = document.querySelector("#last_name");
+const rating = document.querySelector("#rating");
+const hand = document.querySelector("[name='hand']");
+const grip = document.querySelector("[name='grip']");
+const yob = document.querySelector("#yob");
+const height = document.querySelector("#height");
+const notes = document.querySelector("#notes");
+
 /**
  * Fencer object constructor
  * @param {string} firstName 
@@ -35,51 +44,62 @@ const opponents = [];
 
 function addFencer(fencer) {
   opponents.push(fencer);
+  console.log(opponents);
 }
 
 function displayFencers() {
   const fencerList = document.querySelector("#fencer-list");
   fencerList.innerHTML = "";
 
-  opponents.forEach((fencer) => {
+  opponents.forEach((fencer, i) => {
     const newFencer = document.createElement("div");
 
     const fencerInfo = document.createElement("div");
     fencerInfo.innerText = fencer.toString();
     newFencer.appendChild(fencerInfo);
 
-    const editFencer = document.createElement("button");
-    editFencer.innerText = "edit";
-    editFencer.classList.add("edit-fencer");
-    newFencer.appendChild(editFencer);
+    const fencerEdit = editFencer(fencer, i);
+    newFencer.appendChild(fencerEdit);
 
 
     fencerList.appendChild(newFencer);
   })
 }
 
-let submitButton = document.querySelector("#add-an-opponent");
+let fencerForm = document.querySelector("#add-an-opponent");
 
-submitButton.addEventListener("submit", (event) => {
+function editFencer(fencer, i) {
+  const fencerEdit = document.createElement("button");
+  fencerEdit.innerText = "edit";
 
+  fencerEdit.addEventListener("click", (event) => {
+    const fencerToEdit = opponents[i];
 
-  const firstName = document.querySelector("#first_name").value;
-  const lastName = document.querySelector("#last_name").value;
-  const rating = document.querySelector("#rating").value;
-  const hand = document.querySelector("[name='hand']").value;
-  const grip = document.querySelector("[name='grip']").value;
-  const yob = document.querySelector("#yob").value;
-  const height = document.querySelector("#height").value;
-  const notes = document.querySelector("#notes").value;
+    firstName.value = fencerToEdit.firstName;
+    lastName.value = fencerToEdit.lastName;
+    rating.value = fencerToEdit.rating;
+    hand.value = fencerToEdit.hand;
+    grip.value = fencerToEdit.grip;
+    yob.value = fencerToEdit.yob;
+    height.value = fencerToEdit.height;
+    notes.value = fencerToEdit.notes;
 
-  console.log(firstName + lastName + rating + hand + grip + yob + height + notes);
+    opponents.splice(i, 1);
+  })
 
-  const fencer = new Fencer(firstName, lastName, rating, hand, grip, yob, height, notes);
+  return fencerEdit;
+}
+
+fencerForm.addEventListener("submit", (event) => {
+
+  console.log(firstName.value + lastName.value + rating.value + hand.value + grip.value + yob.value + height.value + notes.value);
+
+  const fencer = new Fencer(firstName.value, lastName.value, rating.value, hand.value, grip.value, yob.value, height.value, notes.value);
 
   addFencer(fencer);
   displayFencers();
 
-  submitButton.reset();
+  fencerForm.reset();
 
 
   event.preventDefault();
